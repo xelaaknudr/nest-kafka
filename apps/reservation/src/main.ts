9d8cc42dc515f25fdbfd66b455b2d080d4c71acb
xelaaknudr <andrukovich.a.n@gmail.com>
2026-05-15 15:52:42 +0200

    feat: configure TCP microservice transport and downgrade @nestjs/microservices to version 10.0.0

--- apps/reservation/src/main.ts
+++ apps/reservation/src/main.ts
@@ -2,11 +2,16 @@ import { NestFactory } from '@nestjs/core';
 import { ReservationModule } from './reservation.module';
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
   const app = await NestFactory.create(ReservationModule);
+  app.connectMicroservice({
+    transport: Transport.TCP,
+  });
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   const configService = app.get(ConfigService);
+  await app.startAllMicroservices();
   await app.listen(configService.get('PORT') ?? 3000);
 }
 bootstrap();
